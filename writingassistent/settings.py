356859b278f273790301d851b9ee33b57ca1b34a
Ahmedraza-software <ahmedrazasoftware@gmail.com>
2025-07-10 20:34:36 +0500

    Initial push or updated project with fixes

--- writingassistent/settings.py
+++ writingassistent/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
-import dj_database_url
+import dj_database_url  
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,13 +59,11 @@ WSGI_APPLICATION = 'writingassistent.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-        ssl_require=not DEBUG
-    )
-}
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}   
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
